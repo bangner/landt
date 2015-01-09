28c460f30de1fa093194426f9b8c1d572d23c741
Joe Wagner <joswagne@gmail.com>
2015-01-08 20:19:05 -0600

    first commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,18 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Joe Wagner on 1/8/15.
+//  Copyright (c) 2015 Bangner. All rights reserved.
+//
+
+import Foundation
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
+
+import UIKit    
